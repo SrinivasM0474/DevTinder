59c2da8413c78f55778037836f2026386249ba92
Srinivas Manchikatla <srinivas.manchikatla4@gmail.com>
2025-05-24 19:43:56 -0700

    removed default login creds

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-    const [emailId, setEmailId] = useState("srini@gmail.com");
-    const [password, setPassword] = useState("Srini@123");
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
